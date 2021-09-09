c46dc06c0fbfa8ac768382879995bbfc073743e3
Oleksii Tarasenko <oleksiy.tarasenko@gmail.com>
2021-09-09 15:22:18 +0300

    task 2

--- pokemon-game/src/App.js
+++ pokemon-game/src/App.js
@@ -1,16 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+import Footer from "./components/Footer/Footer";
+import "./index.css";
+import bg from "./assets/bg1.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello React.JS!
-        </p>
-       
-      </header>
+      <Header title="This is title" descr="This is the Description!" />
+      <Layout
+        title="This is the first title"
+        descr="This is the Description!"
+        urlBg={bg}
+      />
+      <Layout
+        title="This is the second title"
+        descr="This is the Description!"
+        colorBg="red"
+      />
+      <Layout
+        title="This is the third title"
+        descr="This is the Description!"
+        urlBg={bg}
+      />
+      <Footer />
     </div>
   );
 }
